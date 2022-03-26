fba7a0eb091f01ef4ea96d695f8203878139acc2
juangb05 <82195788+juangb05@users.noreply.github.com>
2022-03-25 21:07:32 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+// Book Class: Represents a book
+class Book {
+    constructor(title, author, isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+
+
+// UI Class: Handle UI Tasks
+class UI {
+    static displayBooks() {
+        const StoredBooks = [
+            {
+                title: xd,
+            }
+        ]
+    }
+}
+
+
+
+// Store Class: Handle Storage
+
+// Event: Display Books
+
+// Event: Add a book
+
+// Event: Remove a book
+
